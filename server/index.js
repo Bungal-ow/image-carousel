71e963ba6ad6c50ad95ca2dd287b4f2463f13741
Bradley Zazzara <bradleyzazz@gmail.com>
2020-04-07 19:30:19 -0700

    Update index.js to include all CRUD operations

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const handlers = require('./handlers.js')
+const handlers = require('./controller.js')
 const app = express()
 const port = 3001
 const bodyParser = require('body-parser')
 const path = require('path')
+const Controller = require('./controller.js')
 
 
 app.use(express.static(path.resolve(__dirname, '../dist/')));
@@ -11,7 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 
-app.get('/api/property', handlers.handleGet)
+app.get('/api/photos', Controller.getPhotos)
+app.post('/api/photos', Controller.addPhoto)
+app.put('/api/photos', Controller.updatePhoto)
+app.delete('/api/photos', Controller.deletePhoto)
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
